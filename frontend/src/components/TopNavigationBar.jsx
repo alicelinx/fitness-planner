5c228bf74df918382dc7a48768d8cea1e84708d8
Alice Lin <alicexxlin@gmail.com>
2023-08-06 18:41:05 -0600

    refactor

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,9 +1,24 @@
 import '../styles/TopNavigationBar.scss';
 import NavItem from './NavItem';
 
-const TopNavigationBar = () => {
+const TopNavigationBar = ({ setRoute, isLoggedIn, logout }) => {
+
   return (
-    <NavItem />
+    <div className="top-nav-bar">
+
+      <NavItem label="FITNESS PLANNER" toggleRoute={() => setRoute('home')} />
+
+      <NavItem label="Create Workout" />
+
+      <NavItem label="My Workout" />
+
+      {!isLoggedIn && <NavItem label="Login" toggleRoute={() => setRoute('login')} />}
+
+      {isLoggedIn && <NavItem label="Logout" toggleRoute={logout} />}
+
+
+      <NavItem label="Register" />
+    </div>
   );
 };
 
