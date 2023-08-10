c146c33a841cce80626312c53050533c5a16eb4f
Alice Lin <alicexxlin@gmail.com>
2023-08-09 18:31:38 -0600

    implement modal component

--- frontend/src/components/WorkoutItem.jsx
+++ frontend/src/components/WorkoutItem.jsx
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import '../styles/WorkoutItem.scss';
-import Modal from '@mui/material/Modal';
+import Modal from './Modal';
 
 const WorkoutItem = (props) => {
-
-
-
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="workout-item">
-      <h3 className="text-light">{props.title}</h3>
-      <button type="button" className="btn btn-outline-light">Details</button>
-    </div>
+    <>
+      <div className="workout-item">
+        <h3 className="text-light">{props.title}</h3>
+        <button
+          className="btn btn-outline-light"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Details
+        </button>
+      </div>
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
+    </>
   );
 
 
