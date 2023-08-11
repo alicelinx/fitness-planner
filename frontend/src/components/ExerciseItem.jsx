93332ca55e2e63038cce93e124f9a1b609065f5b
Alice Lin <alicexxlin@gmail.com>
2023-08-11 14:32:21 -0600

    refactor

--- frontend/src/components/ExerciseItem.jsx
+++ frontend/src/components/ExerciseItem.jsx
@@ -1,13 +1,13 @@
 import '../styles/WorkoutItem.scss';
 const ExerciseItem = (props) => {
- 
+
 
   return (
     <div className="container workout-item d-flex flex-row justify-content-around align-items-center">
-      <h5>Title: {props.title}</h5>
-      <h5>Reps: {props.reps}</h5>
-      <h5>Sets: {props.sets}</h5>
-      <h5>Weight: {props.weight}</h5>
+      <h5>{props.title}</h5>
+      <h5>{props.reps} reps</h5>
+      <h5>{props.sets} sets</h5>
+      <h5>{props.weight} lbs</h5>
     </div>
   );
 
