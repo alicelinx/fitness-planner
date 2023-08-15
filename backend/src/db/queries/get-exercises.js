6d14207d6f1ca09d7c84ce262247d5e86399bdec
Alice Lin <alicexxlin@gmail.com>
2023-08-14 20:07:21 -0600

    add more data

--- backend/src/db/queries/get-exercises.js
+++ backend/src/db/queries/get-exercises.js
@@ -4,7 +4,7 @@ const getAllExercisesForDropDownMenu = () => {
   return db
     .query(`
     SELECT * FROM exercises
-    WHERE id = ANY (ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+    WHERE id = ANY (ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29])
     ORDER BY title`)
     .then(data => {
       return data.rows;
--- backend/src/db/seeds/02_workouts.sql
+++ backend/src/db/seeds/02_workouts.sql
@@ -4,5 +4,7 @@ INSERT INTO workouts (title, is_complete, date, user_id) VALUES
 ('Chest and Shoulders', true, '2023-08-05 14:45:00', 1),
 ('Full Body Workout', false, '2023-08-04 17:15:00', 1),
 ('Upper Body Strength', false, '2023-08-03 16:00:00', 1),
-('Push Day', false, '2023-08-03 11:30:00', 1);
+('Push Day', false, '2023-08-03 11:30:00', 1),
+('Glutes and Abs', false, '2023-08-03 11:32:00', 1),
+('Back and Triceps', false, '2023-08-03 11:35:00', 1);
 
